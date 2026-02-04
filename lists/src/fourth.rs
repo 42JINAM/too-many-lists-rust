17df35b9825723da1f6a4baae2c2acb0650221ec
42JINAM <jinam@student.42.fr>
2026-02-04 16:52:54 +0100

    drop

--- lists/src/fourth.rs
+++ lists/src/fourth.rs
@@ -69,6 +69,12 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::fourth::List;
