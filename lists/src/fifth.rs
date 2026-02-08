11094a966383c6891f8c9b72b2ee3811d2809e6b
42JINAM <jinam@student.42.fr>
2026-02-08 23:39:35 +0100

    new list and push with unsafe rust

--- lists/src/fifth.rs
+++ lists/src/fifth.rs
@@ -1,7 +1,8 @@
 //queue pops off the other end.
 //FIFO
+//Unsafe Rust
 
-use std::mem;
+use std::{mem, ptr};
 
 pub struct List<T> {
     head: Link<T>,
@@ -16,37 +17,51 @@ struct Node<T> {
     next: Link<T>,
 }
 
-impl<'a, T> List<'a, T> {
+impl<T> List<T> {
     pub fn new() -> Self {
         List {
             head: None,
-            tail: None,
+            tail: ptr::null_mut(),
         }
     }
 
     //as_deref_mut is a helper method on Option that, when the option contains a type
     //implementing DerefMut, converts it into an option of mutable references to the inner target.
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node {
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node {
             elem: elem,
             next: None,
         });
 
         //pub fn push(&'a mut self, elem: T) {
         // old_tail is a local variable,so its lifetime is shorter than self.tail's
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
+        // let new_tail = match self.tail.take() {
+        //     Some(old_tail) => {
+        //         old_tail.next = Some(new_tail);
+        //         old_tail.next.as_deref_mut()
+        //     }
+        //     None => {
+        //         self.head = Some(new_tail);
+        //         self.head.as_deref_mut()
+        //     }
+        // };
+
+        // //Now the compiler can see that the reference stored in new_tail lives as long as self
+        // self.tail = new_tail;
+
+        //unsafe rust
+        let raw_tail: *mut _ = &mut *new_tail;
+        if !self.tail.is_null() {
+            //unsafe tells compiler: I guarantee this raw pointer operation is safe
+            //Required for dereferencing raw pointers and FFI calls
+            unsafe {
+                (*self.tail).next = Some(new_tail);
             }
-            None => {
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
-        //Now the compiler can see that the reference stored in new_tail lives as long as self
-        self.tail = new_tail;
+        } else {
+            self.head = Some(new_tail);
+        }
+        self.tail = raw_tail;
     }
 
     pub fn pop(&'a mut self) -> Option<T> {
